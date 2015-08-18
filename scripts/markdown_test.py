051875a4e5a0ef835e85677412897e7640fee863
nygeog <daniel.martin.sheehan@gmail.com>
2015-08-17 21:16:25 -0400

    update

--- scripts/markdown_test.py
+++ scripts/markdown_test.py
@@ -8,7 +8,7 @@ categories: jekyll update
 
 
 
-text_file = open('/Users/danielmsheehan/GitHub/magnaball.github.com/_posts/2015-08-17-welcome-to-jekyll.markdown', "w")
+text_file = open('/Users/danielmsheehan/GitHub/magnaball.github.com/_posts/2015-08-17-magnaball-stats.markdown', "w")
 
 x = """tickets    98
 dtype: int64"""
